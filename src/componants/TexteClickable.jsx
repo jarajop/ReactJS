d764e499e8bfdc889cabeba82085030b7f947d73
jarajop <diarradiopakd@gmail.com>
2023-08-22 11:35:00 +0000

    style_texte_clikable

--- src/componants/TexteClickable.jsx
+++ src/componants/TexteClickable.jsx
@@ -13,27 +13,30 @@ function TexteClickable() {
   return (
     // ...
     <div className="col-6">
-      <div className="kestion d-flex justify-content-between align-items-center">
-        <p className="p-0 m-0" onClick={toggleOpen}>
-          Question question?
-        </p>
-        <Icon
-          icon="ph:plus-light"
-          color="#908f8f"
-          width="32"
-          height="32"
-          onClick={toggleOpen}
-        />
-      </div>
-      {isOpen && ( 
-      <div className="content kestion d-flex justify-content-between align-items-center">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ab sed
-          iste vero error odio, repellat aperiam officia saepe corrupti
-          veritatis, aliquid delectus doloribus quam! Quia ratione ex amet
-          voluptatum.{" "}
+      <div className="kestion ">
+        <div className="d-flex justify-content-between align-items-center w-100">
+          <p className="p-0 m-0" onClick={toggleOpen}>
+            Question question?
+          </p>
+          <Icon
+            icon="ph:plus-light"
+            color="#908f8f"
+            width="32"
+            height="32"
+            onClick={toggleOpen}
+          />
         </div>
-      )}
+          {isOpen && (
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ab
+              sed iste vero error odio, repellat aperiam officia saepe corrupti
+              veritatis, aliquid delectus doloribus quam! Quia ratione ex amet
+              voluptatum.{" "}
+            </p>
+          )}
+      </div>
     </div>
+
     // ...
   );
 }
